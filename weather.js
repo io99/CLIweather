3c08312a9c7ca836ab5495329306bbd65bf45b07
io99 <31932394+io99@users.noreply.github.com>
2017-09-18 17:39:07 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,7 +1,14 @@
 var request = require('request');
 var url = 'https://api.darksky.net/forecast/457587eefad4aa5cacf9cc91b296cd02/37.8267,-122.4233'
 
-module.exports = function(){
+module.exports = function(location, callback){
+var encodedLocation = encodedURIComponent()
+
+if(!location){
+	return callback('No location provided');
+}
+//if no location is given return callback
+
 request({
 url:url,
 json: true
@@ -17,4 +24,3 @@ json: true
 	}
 });
 }
-
