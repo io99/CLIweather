e924e814e77394c5214b5428e0d93b12d1979baf
io99 <31932394+io99@users.noreply.github.com>
2017-09-28 15:45:58 +0530

    Add files via upload

--- example-promise-2.js
+++ example-promise-2.js
@@ -12,7 +12,7 @@ function doWork(){
 }
 
 doWork().then(function () {
-	return doWork();
+	return doWork(true);
 }).then( function (){
 	console.log('All done');
 }).catch(function (){
@@ -21,3 +21,27 @@ doWork().then(function () {
 //by using then you have access to the promise right after that
 //if doWork doesn't come well then the second promise comes up.
 //if the previous two promises fail, then catch is going to run and catch the error
+//-------------------------------------
+
+function doWork(shouldFail){
+	return new Promise( function (resolve, reject){
+		setTimeout(function (){
+	if(typeof shouldFail== 'boolean'&& shouldFail==true){
+		reject('error message');
+	}else{
+		resolve('success');
+	}
+		}, 1000);
+	});
+}
+
+doWork().then(function (message) {
+	console.log(message);
+	//this message will run if everything goes well. Truth be told it wont go well.
+	return doWork(true);
+}).then( function (message){
+	console.log(message);
+}).catch(function (error){
+	console.log(error);
+});
+
