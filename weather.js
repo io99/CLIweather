ed677c7aeb081c9b50f60e12a9052538012504ef
io99 <31932394+io99@users.noreply.github.com>
2017-10-11 08:29:39 -0700

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,8 +1,10 @@
 var request = require('request');
-var url = 'https://api.darksky.net/forecast/457587eefad4aa5cacf9cc91b296cd02/37.8267,-122.4233'
 
 module.exports = function(location, callback){
-var encodedLocation = encodedURIComponent()
+var encodedLocation = encodedURIComponent(location);
+
+var url = 'https://api.darksky.net/forecast/457587eefad4aa5cacf9cc91b296cd02/37.8267,-122.4233'
+
 
 if(!location){
 	return callback('No location provided');
@@ -13,9 +15,6 @@ request({
 url:url,
 json: true
 }), function (error,response,body){
-	//^this is the callback function. It takes 3 arguements
-	// the first is error
-	// second is respnse and third is the body
 	if(error){
 		console.log('Unable to fetch weather');
 	}
@@ -24,3 +23,14 @@ json: true
 	}
 });
 }
+
+// url encoding
+/*
+location needs to be the first arguement
+
+since we need to build new url everytime
+When your' adding some to the url u need to encodedURIComponentu need to run it through specific function inorder t
+convert spaces and other special characters
+encoding means taking space and converting into computer readable format
+
+encodedURIComponent is a function.  all it needs to take is the location
